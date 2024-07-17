7a14a57438dbd9426dfa3dcdea60f9718d0662e8
helloredreyes <hello.silentlymine@gmail.com>
2024-07-17 21:00:15 +0700

    first commit

---
+++ script/getdates.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const full = document.querySelector("#datetimemodified");
+  
+    const today = new Date();
+  
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "UTC"
+    };
+  
+    const formattedDateTime = new Intl.DateTimeFormat("en-US", options).format(today);
+  
+    full.innerHTML = `Last Modificiation: <span class="highlight">${formattedDateTime}</span>`;
+  });
+  
